cd989575f1479c87e2fc875ac2a1e81ee8e48889
AlvinH19 <alvinragnars@gmail.com>
2018-10-17 10:43:20 +0000

    hallo heimur

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,1 +1,11 @@
-// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(300,300);
+    background(255,180,25);
+    fill(150);
+    ellipse(150,100,50,50);
+    fill(50);
+    textSize(23);
+    text("Halló heimur",90,160);
+    rect(150,50,50,50);
+    ellipse(150,200,50,50);}
+}// Hér kemur kóðinn þinn:
