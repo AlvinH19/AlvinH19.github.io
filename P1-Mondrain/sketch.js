7949a933eafe3b2cfaff410ff97b20997ee9a6db
AlvinH19 <alvinragnars@gmail.com>
2018-10-24 08:35:30 +0000

    málverk?

---
+++ P1-Mondrain/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+    createCanvas(300,300);
+    background(255,180,25);
+    fill(150);
+    ellipse(150,100,50,50);
+    fill(50);
+    textSize(23);
+    text("Halló heimur",90,160);
+    rect(150,50,50,50);
+    ellipse(150,200,50,50);}
