82d8899f728fbbb7dba5429b769e8464c9abbf02
AlvinH19 <alvinragnars@gmail.com>
2018-11-02 13:40:40 +0000

    lykkjulist v1

---
+++ P3-lykkjulist/sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(200,300);
+  background(255, 215, 0);
+  fill(0);
+  colorMode(HSB);
+  frameRate(5);
+}
+
+function draw(){
+  fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(10, 11 + i*20, 15, 15);
+  }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(30, 11 + i*20, 15, 15);
+  }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(50, 11 + i*20, 15, 15);
+     }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(70, 11 + i*20, 15, 15);
+     }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(90, 11 + i*20, 15, 15);
+     }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(110, 11 + i*20, 15, 15);
+     }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(130, 11 + i*20, 15, 15);
+     }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(150, 11 + i*20, 15, 15);
+     }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(170, 11 + i*20, 15, 15);
+     }
+    fill(random(0,360),75,100);
+  for(var i = 0; i < 15 ; i = i + 1) {
+    ellipse(190, 11 + i*20, 15, 15);
+  }
+}
