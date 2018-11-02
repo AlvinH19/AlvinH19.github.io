e214b6b2ee082ead5772ccccc70d2340db68ff6c
AlvinH19 <alvinragnars@gmail.com>
2018-11-02 13:56:12 +0000

    Lykkjulist V3

--- P3-lykkjulist/sketch.js
+++ P3-lykkjulist/sketch.js
@@ -1,7 +1,6 @@
 function setup() {
   createCanvas(300,300);
   background(255, 215, 0);
-  fill(0);
   colorMode(HSB);
   frameRate(5);
 }
@@ -10,7 +9,7 @@ function draw(){
   fill(random(0,360),75,100);
   for(var i = 0; i < 15 ; i = i + 1) {
     ellipse(10, 11 + i*20, 15, 15);
-  }
+   }
     fill(random(0,360),75,100);
   for(var i = 0; i < 15 ; i = i + 1) {
     ellipse(30, 11 + i*20, 15, 15);
