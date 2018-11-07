2291eec41e5d68d7d6be5fc7c968cb313f6e1393
AlvinH19 <alvinragnars@gmail.com>
2018-11-07 10:08:45 +0000

    spraeturV1

---
+++ P5-spraetur/sketch.js
@@ -0,0 +1,15 @@
+var mikkihundur
+var piratepug
+var halloweenparty
+
+function preload() {
+ mikkihundur = loadImage('assets/mikkihundur.png'); // https://petthreads.com.au/wp-content/uploads/2017/01/IMG_2935-600x600.png
+ piratepug = loadImage('assets/piratepug.png'); // https://petthreads.com.au/wp-content/uploads/2017/01/IMG_2958-600x600.png
+halloweenparty = loadImage('assets/halloweenparty.png'); //https://media2.fdncms.com/orlando/imager/u/original/2445361/1000w_drink_plazaween.jpg
+
+}
+
+ function setup() {
+   createCanvas(700,700);
+   background(halloweenparty);
+ }
