478c00b72f98153275fd3b35b9df2bb1ac815551
AlvinH19 <alvinragnars@gmail.com>
2018-11-12 12:31:43 +0000

    skoppV1

--- P5-spraetur/sketch.js
+++ P5-spraetur/sketch.js
@@ -5,11 +5,18 @@ var halloweenparty
 function preload() {
  mikkihundur = loadImage('assets/mikkihundur.png'); // https://petthreads.com.au/wp-content/uploads/2017/01/IMG_2935-600x600.png
  piratepug = loadImage('assets/piratepug.png'); // https://petthreads.com.au/wp-content/uploads/2017/01/IMG_2958-600x600.png
-halloweenparty = loadImage('assets/halloweenparty.png'); //https://media2.fdncms.com/orlando/imager/u/original/2445361/1000w_drink_plazaween.jpg
+halloweenparty = loadImage('assets/halloweenparty.jpg'); //https://media2.fdncms.com/orlando/imager/u/original/2445361/1000w_drink_plazaween.jpg
 
 }
 
  function setup() {
-   createCanvas(700,700);
+   createCanvas(900,700);
    background(halloweenparty);
- }
+
+}
+
+function draw(){
+  image(mikkihundur,300,270);
+  image(piratepug,5,270);
+
+}
---
+++ P6-skopp/sketch.js
@@ -0,0 +1,51 @@
+var x = 100;
+var y = 100;
+var hradiX = 5;
+var hradiY = 11;
+var boltiStaerd = 20;
+var stig = 0;
+var spadiBreidd = 150;
+var spadiThykkt = 25;
+var spadiY;
+
+function preload() {
+bakgrunnur = loadImage('assets/bakgrunnur.jpg'); // https://www.shutterstock.com/ru/video/clip-28633810-animation-grid-80s-retro-sci-fi-background-cyberpunk
+
+}
+
+function setup() {
+	createCanvas(600,500);
+	spadiY = height - 45;
+	rectMode(CENTER);
+	textFont("Menlo",14);
+	textStyle(BOLD);
+
+}
+
+function draw() {
+  background(bakgrunnur);
+	// Reikna ný hnit út frá hraða boltans:
+  x = x + hradiX;
+  y = y + hradiY;
+	// Athuga hvort boltinn snertir veggi:
+  if ((x > width-boltiStaerd/2) || (x < boltiStaerd/2))  {
+    hradiX = hradiX * -1;
+  }
+  if ((y > height-boltiStaerd/2) || (y < boltiStaerd)) {
+    hradiY = hradiY * -1;
+  }
+	// Athuga hvort bolti snertir spaða:
+	// abs() táknar algildi/tölugildi
+	if(abs(spadiY - y) < boltiStaerd/2 + spadiThykkt/2 && abs(mouseX - x) < boltiStaerd/2 + spadiBreidd/2){
+    hradiY = hradiY * -1;
+		stig = stig + 1;
+	}
+	// Teikna boltann, spaðann og stigin
+  colorMode(HSB);
+  fill(280, 204, 100);
+  rect(x, y, boltiStaerd, boltiStaerd);
+	rect(mouseX,spadiY ,spadiBreidd,spadiThykkt);
+	fill('rgba(100%,0%,100%,0.5)');
+	text("Stig: " + stig,20,20);
+
+}
